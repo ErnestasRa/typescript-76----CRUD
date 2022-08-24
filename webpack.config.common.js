3d3a0d533c13c6ccd40e16391aac0b7c455c984d
Ernestas Ragainis <ernestas.ragainis@gmail.com>
2022-08-24 07:43:51 +0200

    feat(1):Crud Task nr.1

---
+++ webpack.config.common.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs',
+      templateParameters: {
+        title: 'TS webpack template',
+      },
+    })],
+};
